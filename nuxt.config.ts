a62176ada56e36d86d403d95d4f3d45a78fbbfa2
Edward Brock <1416477069@qq.com>
2024-11-24 23:59:55 +0800

    chore: 安装 @sidebase/nuxt-auth 依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@sidebase/nuxt-auth'],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-01',
   eslint: {
